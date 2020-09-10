91e40f921a23fffff8a2f41134d91ab85b3ea6d7
Jens <jr@jensravn.com>
2020-09-10 13:17:03 +0200

    fetch device links

--- hue-app/src/App.tsx
+++ hue-app/src/App.tsx
@@ -1,36 +1,78 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 interface HueDevice {
   id: string;
   internalipaddress: string;
+  link?: string;
+  status?: Status;
 }
 
-interface HueDeviceWithLink extends HueDevice {
-  link: string;
+enum Status {
+  LOADING = "STATUS_LOADING",
+  OK = "STATUS_OK",
+  NOT_OK = "STATUS_NOT_OK",
 }
 
 function App() {
-  const [json, setJson] = useState<HueDeviceWithLink[]>([]);
-  fetch("https://discovery.meethue.com/")
-    .then((x) => x.json())
-    .then((x) =>
-      x.map((x: HueDeviceWithLink) => ({
-        ...x,
-        link: `https://${x.internalipaddress}/debug/clip.html`,
-      }))
-    )
-    .then((x) => setJson(x));
+  const [devices, setDevices] = useState<HueDevice[]>([]);
+  const [readyToTestLinks, setReadyToTestLinks] = useState(false);
+
+  useEffect(() => {
+    fetch("https://discovery.meethue.com/")
+      .then((response) => response.json() as Promise<HueDevice[]>)
+      .then((hueDevices) =>
+        hueDevices.map((hueDevice: HueDevice) => ({
+          ...hueDevice,
+          link: `https://${hueDevice.internalipaddress}/debug/clip.html`,
+          status: Status.LOADING,
+        }))
+      )
+      .then((x) => {
+        setDevices(x);
+        setReadyToTestLinks(true);
+      });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (readyToTestLinks) {
+      devices
+        .filter((device) => device.status === Status.LOADING)
+        .map((device) =>
+          fetch(device.link as string)
+            .then((response) =>
+              response.ok
+                ? { ...device, status: Status.OK }
+                : { ...device, status: Status.NOT_OK }
+            )
+            .catch(() => ({ ...device, status: Status.NOT_OK }))
+            .then((device) => {
+              console.log(device.status);
+              setDevices((currentDevices) => [
+                ...currentDevices.filter((x) => x.id !== device.id),
+                device,
+              ]);
+            })
+        );
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [readyToTestLinks]);
 
   return (
     <div>
       <h1>Hue Devices</h1>
       <ul>
-        {json &&
-          json.map((x) => (
+        {devices &&
+          devices.map((x) => (
             <li>
               <p>
-                <span>{x.id}</span>: <a href={x.link}>{x.link}</a>
+                <span>{x.id}</span>:{" "}
+                <a target="blank" href={x.link}>
+                  {x.link}
+                </a>{" "}
+                <span>{x.status}</span>
               </p>
             </li>
           ))}
